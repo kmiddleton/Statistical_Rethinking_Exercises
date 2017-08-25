a261ba601be1d3cbb16386fac2e3bde3df21f3f3
Kevin Middleton <middletonk@missouri.edu>
2017-08-25 16:43:28 -0500

    Add simulated data missing data analysis

--- Impute_Data.R
+++ Impute_Data.R
@@ -1,8 +1,80 @@
-library("rethinking")
-library("tidyverse")
-library("cowplot")
+library(rethinking)
+library(tidyverse)
+library(cowplot)
 
-mammals <- read_delim("~/Dropbox/Classes/Topics_Biostats/data/mammals.txt", delim = "\t")
+###### Simulated data
+
+set.seed(83746)
+n <- 400
+x <- rnorm(n)
+y <- 2 * x + rnorm(n)
+plot(x, y)
+
+prop_missing <- 0.5
+
+fm1 <- map2stan(
+  alist(
+    y ~ dnorm(mu, sigma),
+    mu <- a + b * x,
+    a ~ dnorm(0, 10),
+    b ~ dnorm(0, 10),
+    sigma ~ dcauchy(0, 1)
+  ),
+  data = list(x = x, y = y)
+)
+
+x1 <- x
+missing <- sample(1:n)[1:trunc(prop_missing * n)]
+x1[missing] <- NA
+
+fm2 <- map2stan(
+  alist(
+    y ~ dnorm(mu, sigma),
+    mu <- a + b * x1,
+    x1 ~ dnorm(nu, sigma_nu),
+    a ~ dnorm(0, 10),
+    b ~ dnorm(0, 10),
+    nu ~ dnorm(0, 10),
+    sigma_nu ~ dcauchy(0, 1),
+    sigma ~ dcauchy(0, 1)
+  ),
+  data = list(x1 = x1, y = y)
+)
+
+precis(fm1)
+precis(fm2)
+rethinking::compare(fm1, fm2)
+
+post <- extract.samples(fm2)
+M <- tibble(y = y[missing],
+            x_obs = x[missing],
+            x_imp = apply(post$x1_impute, 2, median))
+ggplot() +
+  geom_point(data = M, aes(x_obs, y), color = "black") +
+  geom_point(data = M, aes(x_imp, y), color = "red")
+
+##
+
+y1 <- y
+y1[1] <- NA
+
+fm3 <- map2stan(
+  alist(
+    yest ~ dnorm(mu, sigma),
+    mu <- a + b * x,
+    yest ~ 
+    a ~ dnorm(0, 10),
+    b ~ dnorm(0, 10),
+    sigma ~ dcauchy(0, 1)
+  ),
+  data = list(x1 = x1, y = y)
+)
+
+
+###### Real data #####################################################
+
+mammals <- read_delim("~/Dropbox/Classes/Topics_Biostats/data/mammals.txt",
+                      delim = "\t")
 names(mammals) <- sub("^[0-9]*-[0-9]*_", "", names(mammals))
 names(mammals) <- sub("MSW05_", "", names(mammals))
 mammals <- dplyr::select(mammals, Order, Binomial, AdultBodyMass_g, 
